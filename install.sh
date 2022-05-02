b3af570de9469c775baad672efbfbbb3ab6b520e
Clarice Bouwer <cbillowes@gmail.com>
2022-05-02 16:32:55 +0400

    Update .vscode terminal tramp stamp

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Drop-in .vscode settings at https://github.com/cbillowes/vscode to present
 echo "                            _        "
 echo "                           | |       "
 echo "   __   _____  ___ ___   __| | ___   "
-echo "   \ \ / / __|/ __/ _ \ / _` |/ _ \  "
+echo "   \ \ / / __|/ __/ _ \ / _  |/ _ \  "
 echo "    \ V /\__ | (_| (_) | (_| |  __/  "
 echo "   (_\_/ |___/\___\___/ \__,_|\___|  "
 
