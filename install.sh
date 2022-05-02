a661139ce8c302a98f8db0507ea2431efe2a4de7
Clarice Bouwer <cbillowes@gmail.com>
2022-05-02 16:36:47 +0400

    Escape character in tramp stamp
    bash: line 8: unexpected EOF while looking for matching ``'
    bash: line 16: syntax error: unexpected end of file

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "                           | |       "
 echo "   __   _____  ___ ___   __| | ___   "
 echo "   \ \ / / __|/ __/ _ \ / _  |/ _ \  "
 echo "    \ V /\__ | (_| (_) | (_| |  __/  "
-echo "   (_\_/ |___/\___\___/ \__,_|\___|  "
+echo "   (_\_/ |___/\___\___/ \__\,_|\___|  "
 
 git clone https://github.com/cbillowes/vscode .vscode
 cd .vscode && rm -rf .git
